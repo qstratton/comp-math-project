d44c55e1ed40189a67b69164b0d3658579049dae
Quinn Stratton <qgstratton@gmail.com>
2018-11-26 21:07:02 -0600

    more data/tests

---
+++ Code/banded-mgs/raspi_bmgs_testing.m
@@ -0,0 +1,39 @@
+1;
+num_trials = 5;
+%dims = [10 50 100 250 500 750 1000];
+dims = [10 500 750 1000];
+range = 1000000; % range for random matrix entries
+% Hold times for mgs.m, banded_mgs.m respectively
+reg_data = [];
+opt_data = [];
+for j = 1:size(dims, 2)
+  step_size = dims(1,j) / 10;
+  bandwidths = 0:step_size:dims(1,j);
+  reg_data = [reg_data; zeros(1,size(bandwidths, 2))];
+  opt_data = [opt_data; zeros(1,size(bandwidths, 2))];
+  for p = 1:size(bandwidths,2);
+    reg_times = []; 
+    opt_times = [];
+    for i = 1:num_trials
+      A = banded(dims(j),dims(j),bandwidths(p),range);
+      %{ Test regular mgs %}
+      tic();
+      mgs(A);
+      reg_time = toc();
+      %{ test banded mgs %}
+      tic();
+      banded_mgs(A,bandwidths(p));
+      opt_time = toc();
+      %{ Add times to respective matrices %}
+      reg_times = [reg_times reg_time];
+      opt_times = [opt_times opt_time];
+    end
+    reg_avg = sum(reg_times) / size(reg_times, 2);
+    opt_avg = sum(opt_times) / size(opt_times, 2);
+    reg_data(j, p) = reg_avg;
+    opt_data(j, p) = opt_avg;
+  end
+end
+
+csvwrite("reg.csv", reg_data);
+csvwrite("opt.csv", opt_data);
